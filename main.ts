4ef60c0829aa8b64c63e06168cd40f40f97ee94b
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2019-01-19 06:23:47 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -235,7 +235,6 @@ namespace mbitbot {
 		PMSPin2 = DigitalPin.P1
 	}
 	serial.redirect(PMSTX,PMSRX,BaudRate.BaudRate9600)
-	basic.pause(1000)
 	serial.onDataReceived("BW", function () {
 		Head = serial.readBuffer(1)
 		if (Head[0] == 66) {
@@ -250,7 +249,6 @@ namespace mbitbot {
 		}
 		led.unplot(4, 0)
         })
-	basic.pause(1000)
 	led.plot(4, 0)
 	pins.setPull(PMSPin1, PinPullMode.PullUp)
 	pins.setPull(PMSPin2, PinPullMode.PullUp)
